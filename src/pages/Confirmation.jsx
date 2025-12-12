1972b89b163f2a3c828023c4103519d20b698de5
handesaurabh <handesaurabh2004@gmail.com>
2025-12-12 12:08:12 +0530

    Create Confirmation.jsx

---
+++ src/pages/Confirmation.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+export default function Confirmation() {
+  const { bookingDetails } = useSelector(state => state.flight);
+
+  return (
+    <div className="page">
+      <h2>Booking Confirmed!</h2>
+
+      <p>Name: {bookingDetails.name}</p>
+      <p>Email: {bookingDetails.email}</p>
+      <p>Flight: {bookingDetails.flight.from} → {bookingDetails.flight.to}</p>
+
+      <Link to="/">
+        <button>Return Home</button>
+      </Link>
+    </div>
+  );
+}
