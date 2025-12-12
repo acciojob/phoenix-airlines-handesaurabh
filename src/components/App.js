6d6360b9a1fa81ccdd346efdce45963c1d0c4f74
handesaurabh <handesaurabh2004@gmail.com>
2025-12-12 13:54:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import FlightSearch from "../pages/FlightSearch";
 import FlightBooking from "../pages/FlightBooking";
@@ -7,14 +7,12 @@ import Confirmation from "../pages/Confirmation";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/flight-search" element={<FlightSearch />} />
-        <Route path="/flight-booking" element={<FlightBooking />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/flight-search" element={<FlightSearch />} />
+      <Route path="/flight-booking" element={<FlightBooking />} />
+      <Route path="/confirmation" element={<Confirmation />} />
+    </Routes>
   );
 }
 
