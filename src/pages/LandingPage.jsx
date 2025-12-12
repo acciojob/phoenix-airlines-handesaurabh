f36e891cf45848f3ee23c1c59510d4816edba725
handesaurabh <handesaurabh2004@gmail.com>
2025-12-12 12:06:42 +0530

    Create LandingPage.jsx

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function LandingPage() {
+  return (
+    <div className="page">
+      <h1>Phoenix Airlines</h1>
+      <Link to="/flight-search">
+        <button className="book-flight">Search Flights</button>
+      </Link>
+    </div>
+  );
+}
