af58b098400c7fb712ff7addfc0e58be97e45a72
handesaurabh <handesaurabh2004@gmail.com>
2025-12-12 13:29:30 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
   return (
     <div className="page">
-      <h1>Phoenix Airlines</h1>
+      <h1>Flight Booking App</h1>
+      <h2>Phoenix Airlines</h2>
       <Link to="/flight-search">
         <button className="book-flight">Search Flights</button>
       </Link>
