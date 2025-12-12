fa32a00d90dc11f7c2601f5885ae91ed46a07f37
handesaurabh <handesaurabh2004@gmail.com>
2025-12-12 12:32:08 +0530

    Delete src/pages/backend directory

--- src/pages/backend/data/flights.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-  { id: 1, airline: "Phoenix Airlines", from: "Delhi", to: "Mumbai", date: "2025-01-10", price: 4500 },
-  { id: 2, airline: "Phoenix Airlines", from: "Mumbai", to: "Delhi", date: "2025-01-10", price: 4700 },
-  { id: 3, airline: "Phoenix Airlines", from: "Pune", to: "Bangalore", date: "2025-01-15", price: 3500 }
-];
