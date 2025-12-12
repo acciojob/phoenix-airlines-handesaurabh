58ae2a7bc5ec5132adce14439dc2ee45b94b6a85
handesaurabh <handesaurabh2004@gmail.com>
2025-12-12 12:31:17 +0530

    Delete src/pages/backend directory

--- src/pages/backend/routes/flightRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { searchFlights } = require("../controllers/flightController");
-
-router.get("/search", searchFlights);
-
-module.exports = router;
