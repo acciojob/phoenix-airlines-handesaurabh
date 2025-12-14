a7e57064dbc4bdd8d20bd6593f4a099e752ed1b8
handesaurabh <handesaurabh2004@gmail.com>
2025-12-14 20:28:39 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 export default function LandingPage() {
-  return (
-    <div className="page">
-      <h1>Welcome to Flight Booking App</h1>
-      <h2>Phoenix Airlines</h2>
-      <Link to="/flight-search">
-        <button className="book-flight">Search Flights</button>
-      </Link>
-    </div>
-  );
+const navigate = useNavigate();
+
+
+return (
+<div className="container">
+<h2>Phoenix Airlines</h2>
+<button onClick={() => navigate("/flight-search")}>
+Book Flight
+</button>
+</div>
+);
 }
