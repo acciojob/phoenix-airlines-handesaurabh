ebabe2bdc5d4a5d33d06eeddeeafa0d199371002
handesaurabh <handesaurabh2004@gmail.com>
2025-12-14 20:26:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "../pages/LandingPage";
-import FlightSearch from "../pages/FlightSearch";
-import FlightBooking from "../pages/FlightBooking";
-import Confirmation from "../pages/Confirmation";
+import LandingPage from "./pages/LandingPage";
+import FlightSearch from "./pages/FlightSearch";
+import FlightBooking from "./pages/FlightBooking";
+import Confirmation from "./pages/Confirmation";
+
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/flight-search" element={<FlightSearch />} />
-      <Route path="/flight-booking" element={<FlightBooking />} />
-      <Route path="/confirmation" element={<Confirmation />} />
-    </Routes>
-  );
+return (
+<Routes>
+<Route path="/" element={<LandingPage />} />
+<Route path="/flight-search" element={<FlightSearch />} />
+<Route path="/flight-booking" element={<FlightBooking />} />
+<Route path="/confirmation" element={<Confirmation />} />
+</Routes>
+);
 }
+
 
 export default App;
