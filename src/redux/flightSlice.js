42656e80cf600b045fbd0244121da75482964e17
handesaurabh <handesaurabh2004@gmail.com>
2025-12-14 20:27:52 +0530

    Update flightSlice.js

--- src/redux/flightSlice.js
+++ src/redux/flightSlice.js
@@ -1,27 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const initialState = {
-  search: {},
-  selectedFlight: null,
-  bookingDetails: null
+search: {},
+selectedFlight: null,
+bookingDetails: null
 };
 
+
 const flightSlice = createSlice({
-  name: "flight",
-  initialState,
-  reducers: {
-    setSearchDetails: (state, action) => {
-      state.search = action.payload;
-    },
-    setSelectedFlight: (state, action) => {
-      state.selectedFlight = action.payload;
-    },
-    setBookingDetails: (state, action) => {
-      state.bookingDetails = action.payload;
-    }
-  }
+name: "flight",
+initialState,
+reducers: {
+setSearchDetails: (state, action) => {
+state.search = action.payload;
+},
+setSelectedFlight: (state, action) => {
+state.selectedFlight = action.payload;
+},
+setBookingDetails: (state, action) => {
+state.bookingDetails = action.payload;
+}
+}
 });
 
-export const { setSearchDetails, setSelectedFlight, setBookingDetails } = flightSlice.actions;
-export default flightSlice.reducer;
 
+export const {
+setSearchDetails,
+setSelectedFlight,
+setBookingDetails
+} = flightSlice.actions;
+
+
+export default flightSlice.reducer;
