367c7a4669927bcc815d5e21170620084dc74734
handesaurabh <handesaurabh2004@gmail.com>
2025-12-14 23:05:23 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,7 @@
 import "jest-enzyme";
 import "jest-styled-components";
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 const adapter = new Adapter();
 
