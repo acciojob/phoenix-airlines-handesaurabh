4fd294a5676de4bf6d8e45640852af1d87bd69bc
handesaurabh <handesaurabh2004@gmail.com>
2025-12-15 22:48:15 +0530

    Update home.jsx

--- src/modules/home/home.jsx
+++ src/modules/home/home.jsx
@@ -1,19 +1,52 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
 
-import FlightBooking from "../booking/flight-booking";
+import { Route, Switch } from "react-router-dom";
+import { Grid, CssBaseline, Container, Toolbar } from "@material-ui/core";
+
+import "../../styles.css";
+
+import Header from "../../components/header/header";
+import ErrorBoundaries from "../../components/error/error";
 import Confirmation from "../confirmation/confirmation";
-import FlightSearch from "../search/flight-search";
-import Dashboard from "./dashboard";
+const Dashboard = lazy(() => import("./dashboard"));
+const FlightSearch = lazy(() => import("../search/flight-search"));
+const FlightBooking = lazy(() => import("../booking/flight-booking"));
 
 const Home = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Dashboard} />
-      <Route path="/search" component={FlightSearch} />
-      <Route path="/booking" component={FlightBooking} />
-      <Route path="/confirmation" component={Confirmation} />
-    </Switch>
+    <div className="root">
+      <CssBaseline />
+      <Header />
+      <Toolbar />
+      <Container>
+        <Grid container styles={{ marginTop: 100 }}>
+          <Grid item xs={12} sm={12}>
+            <ErrorBoundaries>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                  <Route path={`/`} exact={true} component={Dashboard} />
+                  <Route
+                    exact={true}
+                    path={`/flight-search`}
+                    component={FlightSearch}
+                  />
+                  <Route
+                    exact={true}
+                    path={`/flight-booking`}
+                    component={FlightBooking}
+                  />
+                  <Route
+                    exact={true}
+                    path={`/confirmation`}
+                    component={Confirmation}
+                  />
+                </Switch>
+              </Suspense>
+            </ErrorBoundaries>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
   );
 };
 
