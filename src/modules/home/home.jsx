95771335e8a30b2bc61a245f3af81397f77b4ed5
handesaurabh <handesaurabh2004@gmail.com>
2025-12-15 22:45:20 +0530

    Update home.jsx

--- src/modules/home/home.jsx
+++ src/modules/home/home.jsx
@@ -1,52 +1,19 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import { Route, Switch } from "react-router-dom";
-import { Grid, CssBaseline, Container, Toolbar } from "@material-ui/core";
-
-import "../../styles.css";
-
-import Header from "../../components/header/header";
-import ErrorBoundaries from "../../components/error/error";
+import FlightBooking from "../booking/flight-booking";
 import Confirmation from "../confirmation/confirmation";
-const Dashboard = lazy(() => import("./dashboard"));
-const FlightSearch = lazy(() => import("../search/flight-search"));
-const FlightBooking = lazy(() => import("../booking/flight-booking"));
+import FlightSearch from "../search/flight-search";
+import Dashboard from "./dashboard";
 
 const Home = () => {
   return (
-    <div className="root">
-      <CssBaseline />
-      <Header />
-      <Toolbar />
-      <Container>
-        <Grid container styles={{ marginTop: 100 }}>
-          <Grid item xs={12} sm={12}>
-            <ErrorBoundaries>
-              <Suspense fallback={<div>Loading...</div>}>
-                <Switch>
-                  <Route path={`/`} exact={true} component={Dashboard} />
-                  <Route
-                    exact={true}
-                    path={`/flight-search`}
-                    component={FlightSearch}
-                  />
-                  <Route
-                    exact={true}
-                    path={`/flight-booking`}
-                    component={FlightBooking}
-                  />
-                  <Route
-                    exact={true}
-                    path={`/confirmation`}
-                    component={Confirmation}
-                  />
-                </Switch>
-              </Suspense>
-            </ErrorBoundaries>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/search" component={FlightSearch} />
+      <Route path="/booking" component={FlightBooking} />
+      <Route path="/confirmation" component={Confirmation} />
+    </Switch>
   );
 };
 
